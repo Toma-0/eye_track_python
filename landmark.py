6e56fd1d55a4d9c49323fc9abbf685d19f4ac43b
Toma-0 <69076412+Toma-0@users.noreply.github.com>
2022-10-05 21:41:51 +0900

    Delete landmark.py

--- landmark.py
+++
@@ -1,6 +0,0 @@
-#ランドマーク　import準備
-import dlib
-
-face_detector = dlib.get_frontal_face_detector()
-predictor_path = 'shape_predictor_68_face_landmarks.dat'
-face_predictor = dlib.shape_predictor(predictor_path)
